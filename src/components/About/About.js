44485d8b6a21104ba0adace2b1bea99f7a9212e7
safuvan <contactmesafwan@gmail.com>
2024-10-09 16:28:25 +0530

    initial commit

---
+++ src/components/About/About.js
@@ -0,0 +1,72 @@
+import React from "react";
+import { stackList } from "../../data/ProjectData";
+import {
+  Image,
+  Technologies,
+  Tech,
+  TechImg,
+  TechName,
+  ContactWrapper,
+} from "./AboutElements";
+import ScrollAnimation from "react-animate-on-scroll";
+function About() {
+  return (
+    <ContactWrapper id="about">
+      <div className="Container">
+        <div className="SectionTitle">About Me</div>
+        <div className="BigCard">
+          <ScrollAnimation animateIn="fadeInLeft">
+            <Image src="/man-svgrepo-com.svg" alt="man-svgrepo" />
+          </ScrollAnimation>
+          <div className="AboutBio">
+            <ScrollAnimation animateIn="fadeInLeft">
+              Hello! My name is <strong>Mohamed Safuvan</strong>. As a
+              self-driven developer, I've turned my passion for technology into
+              a dynamic career in software development, continuously learning
+              and leveraging hands-on experience. I’m always eager to dive into
+              new technologies and explore their potential.
+            </ScrollAnimation>
+
+            <br />
+            <br />
+
+            <ScrollAnimation animateIn="fadeInLeft">
+              My journey into the world of technology began, After completing my
+              Plus Two education, I chose a different path. Instead of pursuing
+              a college degree, I dedicated over a year to mastering
+              cutting-edge programming technologies and tools. This intense
+              learning experience equipped me with practical skills that rival
+              or even exceed those of many computer science graduates.
+            </ScrollAnimation>
+
+            <br />
+            <br />
+
+            <ScrollAnimation animateIn="fadeInLeft">
+              Working collaboratively within a team towards a shared objective
+              has been an incredibly rewarding and unique experience for me. I
+              am eager to continue exploring exciting projects in the future,
+              with a particular interest in web development.
+              <div className="tagline2">
+                I have become confident using the following technologies:
+              </div>
+            </ScrollAnimation>
+
+            <Technologies>
+              {stackList.map((stack, index) => (
+                <ScrollAnimation animateIn="fadeInLeft" key={index}>
+                  <Tech key={index} className="tech">
+                    <TechImg src={stack.img} alt="" />
+                    <TechName>{stack.name}</TechName>
+                  </Tech>
+                </ScrollAnimation>
+              ))}
+            </Technologies>
+          </div>
+        </div>
+      </div>
+    </ContactWrapper>
+  );
+}
+
+export default About;
